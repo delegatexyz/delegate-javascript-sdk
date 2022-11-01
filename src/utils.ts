6b60cd8c9fb2326e0ebd2979f600031f880771c3
ryeshrimp <ryeshrimp@protonmail.com>
2022-11-01 17:20:20 -0400

    Move reads/writes into a class for custom providers

--- src/utils.ts
+++ src/utils.ts
@@ -5,15 +5,16 @@ export const getDelegationType = (key: number) => {
   return types[key] || 'NONE';
 };
 
-export const getProvider = () => {
+export const getProvider = (): ethers.providers.Web3Provider | ethers.providers.JsonRpcProvider => {
+  
   if (typeof process !== 'undefined' && process.env?.PROVIDER_URL)
     return new ethers.providers.JsonRpcProvider(process.env.PROVIDER_URL);
-  // @ts-ignore
-  if (typeof window === 'undefined') return new ethers.providers.getDefaultProvider();
-  // @ts-ignore
+  
+    // @ts-ignore
   if (window.ethereum) {
     // @ts-ignore
     return new ethers.providers.Web3Provider(window.ethereum);
   }
-  return ethers.providers.getDefaultProvider();
+
+  return null;
 };
