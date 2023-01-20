ef2f7b2d0e50b7b2ac8a9faef0edd1ed07e6807a
ryeshrimp <ryeshrimp@protonmail.com>
2023-01-20 11:40:40 -0500

    Fix bug where error throws on backend implementations

--- src/utils.ts
+++ src/utils.ts
@@ -11,7 +11,7 @@ export const getDefaultProvider = (): ethers.providers.Web3Provider | ethers.pro
     return new ethers.providers.JsonRpcProvider(process.env.PROVIDER_URL);
   
     // @ts-ignore
-  if (window.ethereum) {
+  if (typeof window !== 'undefined' && window.ethereum) {
     // @ts-ignore
     return new ethers.providers.Web3Provider(window.ethereum);
   }
